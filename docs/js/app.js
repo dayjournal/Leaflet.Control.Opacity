16eaf4504ccd51c1bef10dd81e1a9caa29105c7b
dayjournal <yasunorikirimoto@gmail.com>
2021-03-21 21:35:23 +0900

    :recycle: フォーマット
    - :recycle: フォーマット

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,27 +1,30 @@
-
 //MIERUNE Color
-const m_color = new L.tileLayer("https://tile.mierune.co.jp/mierune/{z}/{x}/{y}.png", {
-    attribution: "Maptiles by <a href='http://mierune.co.jp/' target='_blank'>MIERUNE</a>, under CC BY. Data by <a href='http://osm.org/copyright' target='_blank'>OpenStreetMap</a> contributors, under ODbL."
+const m_color = new L.tileLayer('https://tile.mierune.co.jp/mierune/{z}/{x}/{y}.png', {
+    attribution:
+        "Maptiles by <a href='http://mierune.co.jp/' target='_blank'>MIERUNE</a>, under CC BY. Data by <a href='http://osm.org/copyright' target='_blank'>OpenStreetMap</a> contributors, under ODbL.",
 });
 
 //MIERUNE MONO
-const m_mono = new L.tileLayer("https://tile.mierune.co.jp/mierune_mono/{z}/{x}/{y}.png", {
-    attribution: "Maptiles by <a href='http://mierune.co.jp/' target='_blank'>MIERUNE</a>, under CC BY. Data by <a href='http://osm.org/copyright' target='_blank'>OpenStreetMap</a> contributors, under ODbL."
+const m_mono = new L.tileLayer('https://tile.mierune.co.jp/mierune_mono/{z}/{x}/{y}.png', {
+    attribution:
+        "Maptiles by <a href='http://mierune.co.jp/' target='_blank'>MIERUNE</a>, under CC BY. Data by <a href='http://osm.org/copyright' target='_blank'>OpenStreetMap</a> contributors, under ODbL.",
 });
 
 //OSM
 const o_std = new L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
 });
 
 //GSI Pale
 const t_pale = new L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png', {
-    attribution: "<a href='http://www.gsi.go.jp/kikakuchousei/kikakuchousei40182.html' target='_blank'>国土地理院</a>",
+    attribution:
+        "<a href='http://www.gsi.go.jp/kikakuchousei/kikakuchousei40182.html' target='_blank'>国土地理院</a>",
 });
 
 //GSI Ort
 const t_ort = new L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/ort/{z}/{x}/{y}.jpg', {
-    attribution: "<a href='http://www.gsi.go.jp/kikakuchousei/kikakuchousei40182.html' target='_blank'>国土地理院</a>",
+    attribution:
+        "<a href='http://www.gsi.go.jp/kikakuchousei/kikakuchousei40182.html' target='_blank'>国土地理院</a>",
 });
 
 //MAP
@@ -29,35 +32,32 @@ const map = L.map('map', {
     center: [35.6831925, 139.7511307],
     zoom: 13,
     zoomControl: true,
-    layers: [m_mono]
+    layers: [m_mono],
 });
 
 //BaseLayer
 const Map_BaseLayer = {
-    "MIERUNE Color": m_color,
-    "MIERUNE MONO": m_mono
+    'MIERUNE Color': m_color,
+    'MIERUNE MONO': m_mono,
 };
 
 //AddLayer
 const Map_AddLayer = {
-    "OSM": o_std,
-    "GSI Pale": t_pale,
-    "GSI Ort": t_ort
+    OSM: o_std,
+    'GSI Pale': t_pale,
+    'GSI Ort': t_ort,
 };
 
 //LayerControl
-L.control.layers(
-    Map_BaseLayer,
-    Map_AddLayer,
-    {
-    collapsed: false
-    }
-).addTo(map);
+L.control
+    .layers(Map_BaseLayer, Map_AddLayer, {
+        collapsed: false,
+    })
+    .addTo(map);
 
 //OpacityControl
-L.control.opacity(
-    Map_AddLayer,
-    {
-    label: "Layers Opacity"
-    }
-).addTo(map);
+L.control
+    .opacity(Map_AddLayer, {
+        label: 'Layers Opacity',
+    })
+    .addTo(map);
